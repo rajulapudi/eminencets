339d6b5aee2be9204115d80926f4bf60e731498a
Rajulapudi Mohan praneeth <rajulapudi@Rajulapudis-MacBook-Pro.local>
2020-09-16 18:41:56 +0530

    alerts and toasts

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,39 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var formidable = require('formidable');
-var fs = require('fs');
-var path = require('path');
-
+var formidable = require("formidable");
+var fs = require("fs");
+var path = require("path");
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('home');
+router.get("/", function (req, res, next) {
+  res.render("home");
 });
 
-router.get('/careers', function (req, res, next) {
-  res.render('careers', { info: "" });
+router.get("/careers", function (req, res, next) {
+  res.render("careers", { info: "" });
 });
 
-router.post('/resume', function (req, res, next) {
+router.post("/resume", function (req, res, next) {
   const form = new formidable.IncomingForm();
   form.parse(req, function (err, fields, files) {
-    const { name, email, skills, contact } = fields
-    var oldPath = files.resume.path;
-    var newPath = path.join(__dirname, '../', 'uploads')
-      + '/' + files.resume.name
-    var rawData = fs.readFileSync(oldPath)
-    fs.writeFile(newPath, rawData, function (err) {
-      if (err) {
-        console.log(err)
-      } else {
-        res.render('careers', { info: "Success" });
-      }
-
-    })
-  })
+    const { name, email, skills, contact } = fields;
+    console.log(files);
+    if (files.resume.name) {
+      var oldPath = files.resume.path;
+      var newPath =
+        path.join(__dirname, "../", "uploads") + "/" + files.resume.name;
+      var rawData = fs.readFileSync(oldPath);
+      fs.writeFile(newPath, rawData, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("careers", { info: "Success" });
+        }
+      });
+    } else {
+      res.render("careers", { info: "Success" });
+    }
+  });
 });
-
 
 module.exports = router;
